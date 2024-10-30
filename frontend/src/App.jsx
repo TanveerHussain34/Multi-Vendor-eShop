5e11180955cb54cdaf6334ae8bf4c4e49b63a41c
Tanveer Hussain <maliktanveerhussain459@gmail.com>
2024-10-30 14:57:24 +0500

    Login Page Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,9 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 className="text-3xl font-bold text-cyan-600">App.jsx</h1>
+      <h1>App.jsx</h1>
     </>
   );
 }
