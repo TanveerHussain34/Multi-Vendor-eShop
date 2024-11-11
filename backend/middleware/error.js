927f0640297eb1fae1aba7dfca319895615d2879
Tanveer Hussain <maliktanveerhussain459@gmail.com>
2024-11-11 09:14:22 +0500

    working on auth

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -15,4 +15,21 @@ module.exports = (err, req, res, next) => {
     const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
     err = new ErrorHandler(message, 400);
   }
+
+  // wrong jwt error
+  if (err.name === "JsonWebTokenError") {
+    const message = "JSON Web Token or Url is invalid. Try again!";
+    err = new ErrorHandler(message, 400);
+  }
+
+  // wrong jwt expired error
+  if (err.name === "TokenExpiredError") {
+    const message = "JSON Web Token or Url is expired. Try again!";
+    err = new ErrorHandler(message, 400);
+  }
+
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
 };
