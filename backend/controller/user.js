c9b8a855e4933c5f04d1dea4d94d869475d1fd5b
Tanveer Hussain <maliktanveerhussain459@gmail.com>
2025-01-01 23:13:48 +0500

    Struggling with the MongoDB connection

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -11,51 +11,74 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const user = require("../model/user");
 const sendToken = require("../utils/jwtToken");
 
-router.post("/create-user", async (req, res, next) => {
-  console.log(req.body);
-  try {
-    const { name, email, password, avatar } = req.body;
-    const userEmail = await User.findOne({ email });
+router.post("/create-user", upload.single("file"), async (req, res) => {
+  const { name, email, password } = req.body;
+  const userEmail = await User.fineOne({ email });
 
-    if (userEmail) {
-      return next(new ErrorHandler("User already exists", 400));
-    }
-
-    const filename = req.file.filename;
-    const fileUrl = path.join("uploads", filename);
-    const user = {
-      name: name,
-      email: email,
-      password: password,
-      avatar: {
-        public_id: filename,
-        url: fileUrl,
-      },
-    };
-
-    console.log("User:", user);
-
-    // const activationToken = createActivationToken(user);
-
-    // const activationUrl = `http://localhost:5173/activation/${activationToken}`;
-
-    // try {
-    //   await sendMail({
-    //     email: user.email,
-    //     subject: "Activate your account",
-    //     message: `Hello ${user.name}, please click on the link to activate your account: ${activationUrl}`,
-    //   });
-    //   res.status(201).json({
-    //     success: true,
-    //     message: `Please check your email:- ${user.email} to activate your account`,
-    //   });
-    // } catch (error) {
-    //   return next(new ErrorHandler(error.message, 500));
-    // }
-  } catch (error) {
-    return next(new ErrorHandler(error.message, 400));
+  if (userEmail) {
+    return next(new ErrorHandler("User already exists", 400));
   }
+
+  const filename = req.file.fieldname;
+  const fileUrl = path.join("uploads", filename);
+
+  const user = {
+    name: name,
+    email: email,
+    password: password,
+    avatar: {
+      public_id: __filename,
+      url,
+      fileUrl,
+    },
+  };
 });
+
+// router.post("/create-user", async (req, res, next) => {
+//   console.log(req.body);
+//   try {
+//     const { name, email, password, avatar } = req.body;
+//     const userEmail = await User.findOne({ email });
+
+//     if (userEmail) {
+//       return next(new ErrorHandler("User already exists", 400));
+//     }
+
+//     const filename = req.file.filename;
+//     const fileUrl = path.join("uploads", filename);
+//     const user = {
+//       name: name,
+//       email: email,
+//       password: password,
+//       avatar: {
+//         public_id: filename,
+//         url: fileUrl,
+//       },
+//     };
+
+// console.log("User:", user);
+
+// const activationToken = createActivationToken(user);
+
+// const activationUrl = `http://localhost:5173/activation/${activationToken}`;
+
+// try {
+//   await sendMail({
+//     email: user.email,
+//     subject: "Activate your account",
+//     message: `Hello ${user.name}, please click on the link to activate your account: ${activationUrl}`,
+//   });
+//   res.status(201).json({
+//     success: true,
+//     message: `Please check your email:- ${user.email} to activate your account`,
+//   });
+// } catch (error) {
+//   return next(new ErrorHandler(error.message, 500));
+// }
+//   } catch (error) {
+//     return next(new ErrorHandler(error.message, 400));
+//   }
+// });
 
 // create activation token
 // const createActivationToken = (user) => {
