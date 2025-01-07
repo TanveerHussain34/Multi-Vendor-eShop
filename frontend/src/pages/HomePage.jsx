9e58a2960095561e80661a4250d0b32f850f20e7
Tanveer Hussain <maliktanveerhussain459@gmail.com>
2025-01-07 20:59:14 +0500

    Adding categories section

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Layout/Header.jsx";
 import Hero from "../components/Route/Hero/Hero.jsx";
+import Categories from "../components/Route/Categories/Categories.jsx";
 
 function HomePage() {
   return (
     <div>
       <Header activeHeading={1} />
       <Hero />
+      <Categories />
     </div>
   );
 }
