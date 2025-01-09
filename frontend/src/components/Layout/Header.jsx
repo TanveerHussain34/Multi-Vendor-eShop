04bcf3805e075f48460e4c8d3da38621b79ab34f
Tanveer Hussain <maliktanveerhussain459@gmail.com>
2025-01-09 20:52:03 +0500

    Best Deals Component Done

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -34,7 +34,7 @@ function Header({ activeHeading }) {
   };
 
   window.addEventListener("scroll", () => {
-    if (window.screenY > 70) {
+    if (window.scrollY > 70) {
       setActive(true);
     } else {
       setActive(false);
@@ -108,7 +108,7 @@ function Header({ activeHeading }) {
           className={`${styles.section} relative ${styles.normalFlex} justify-between`}
         >
           {/* categories */}
-          <div>
+          <div onClick={() => setDropDown(!dropDown)}>
             <div className="relative h-[60px] mt-[10px] w-[270px] hidden 1000px:block">
               <BiMenuAltLeft size={30} className="absolute top-3 left-2" />
               <button className="h-[100%] w-full flex justify-between items-center pl-10 bg-white font-sans text-lg font-[500] select-none rounded-t-md">
